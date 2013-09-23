aa1d262a65441cea760ad146234f0547f56d2785
hirokiky <hirokiky@gmail.com>
2013-09-23 12:55:03 +0900

    Followed to a corresponding to docutils 0.11 on django's core.
    refs pull-request #324
    https://github.com/django/django/commit/a3a59a3197d4e03c5c016edc581bfc5f25668bca

--- _ext/djangodocs.py
+++ _ext/djangodocs.py
@@ -106,11 +106,14 @@ class DjangoHTMLTranslator(SmartyPantsHTMLTranslator):
 
     # Don't use border=1, which docutils does by default.
     def visit_table(self, node):
+        self.context.append(self.compact_p)
+        self.compact_p = True
         self._table_row_index = 0 # Needed by Sphinx
         self.body.append(self.starttag(node, 'table', CLASS='docutils'))
 
     # avoid error with docutils 0.11 or later
     def depart_table(self, node):
+        self.compact_p = self.context.pop()
         self.body.append('</table>\n')
 
     # <big>? Really?
